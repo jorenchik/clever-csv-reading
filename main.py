2ef6d78acbb8df1baaa269c515f3eccf7a81f974
Jorens Shtekels <jorens.stekels@gmail.com>
2024-02-25 15:23:16 +0200

    finished score counting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 import re
+import math
 
 ARRAY_COUNT = 40
 EXCLUDE_QUESION_INDEXES = [5]
@@ -36,20 +37,30 @@ with open('weights.csv', newline='') as csvfile:
 respondent_scores = []
 
 EXCLUDE_INDEXES = [5, 8, 12, 14, 16, 18, 20, 24]
-for row in question_rows:
-    score = 0
-    for i, answer_str in enumerate(row):
-        answers = answer_str.split(";")
-        question = col_questions[i]
-        if i in EXCLUDE_INDEXES:
-            continue
-        for k, answer in enumerate(answers):
-            if answer == "":
+
+with open('result.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["indekss"] + col_questions + ["rezultāts"])
+    for idx, row in enumerate(question_rows):
+        score = 0
+        score_row = []
+        for i, answer_str in enumerate(row):
+            answers = answer_str.split(";")
+            question = col_questions[i]
+            if i in EXCLUDE_INDEXES:
+                score_row.append(0)
                 continue
-            if re.match(r'Cits: (\d)', answer):
-                score = int(re.sub(r'Cits: (\d)', r'\g<1>', answer))
-            else:
-                score += int(answer_weights[question][answer])
+            answer_score = 0
+            for k, answer in enumerate(answers):
+                if answer == "":
+                    continue
+                if re.match(r'Cits: (\d)', answer):
+                    answer_score = int(re.sub(r'Cits: (\d)', r'\g<1>', answer))
+                else:
+                    answer_score += int(answer_weights[question][answer])
+            score += answer_score
+            score_row.append(answer_score)
+        writer.writerow([idx + 1] + score_row + [sum(score_row)])
 
 # SELECTED_QUESTION = 32
 # for answer in question_answers[col_questions[SELECTED_QUESTION]]:
